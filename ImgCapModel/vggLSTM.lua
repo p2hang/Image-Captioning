9149ca44944db29c8d5d403ad309ed68517de5e8
plzhang <plzhang2013@gmail.com>
2016-12-04 17:24:49 -0500

    updated forward and backward

--- ImgCapModel/vggLSTM.lua
+++ ImgCapModel/vggLSTM.lua
@@ -6,47 +6,52 @@ local vggLSTM, parent = torch.class('ImgCap.vggLSTM', 'nn.Module')
 
 function vggLSTM:__init(config)
 
-	self.vocab = ld:loadVocab()
-	self.ivocab = ld:loadiVocab()
-	self.num_words = #ivocab
-	self.embedding_vec = nn.LookupTable(config.num_words, config.embeddingDim)
-	self.embedding_vec.weight:copy(ld:loadVocab2Emb())
-	self.loadImageModel(config.prototxt, config.binary) -- load the image model
+    self.vocab = ld:loadVocab()
+    self.ivocab = ld:loadiVocab()
+    self.num_words = #ivocab
+    self.embedding_vec = nn.LookupTable(config.num_words, config.embeddingDim)
+    self.embedding_vec.weight:copy(ld:loadVocab2Emb())
 
-	self.visualFeature = nn.Sequential()
-					:add(nn.Linear(4096, self.embeddingDim))
+    -- load image model after remove the last few layers
+    local imageModel = loadcaffe.load(prototxt, binary)
 
-	-- let the LSTM unit share the output of the visual feature after rescale
-	local lstmCell = nn.LSTM(config.embeddingDim, config.embeddingDim, 60)
-	lstmCell.prevOutput = self.visualFeature.output
+    -- remove the layers after Linear(4096 -> 4096), 43-46
+    for i in { 46, 45, 44, 43 } do
+        imageModel:remove(i)
+    end
+    self.imageModel = imageModel
 
+    -- visual rescale layer
+    self.visualRescale = nn.Sequential()
+            :add(nn.Linear(4096, self.embeddingDim))
 
-	local rnnUnit = nn.Sequential()
-					:add(lstmCell)
-					:add(nn.Linear(config.embeddingDim, config.num_words))
-	self.LSTM = nn.Sequencer(rnnUnit)
+    -- language model.
+    local LSTMcell = nn.Sequential()
+            :add(nn.LSTM(config.embeddingDim, config.embeddingDim, 60))
+            :add(nn.Linear(config.embeddingDim, config.num_words))
+    self.LSTM = nn.Sequencer(LSTMcell)
 end
 
-function vggLSTM:loadImageModel(prototxt, binary)
-	local imageModel = loadcaffe.load(prototxt, binary)
-
-	-- remove the layers after Linear(4096 -> 4096), 43-46
-	imageModel:remove(46)
-	imageModel:remove(45)
-	imageModel:remove(44)
-	imageModel:remove(43)
-	self.imageModel = imageModel
-end
 
 function vggLSTM:forward(input)
-	-- image nets
-	self.image_representation = self.vgg19:forward(input)
-	self.
-	-- LSTM
-	self.output = self.LSTM:forward(self.image_representation)
-	return self.output
+    -- image nets
+    self.outputImageModel = self.imageModel:forward(input)
+    self.visualFeatureRescaled = self.visualRescale:forward(self.outputImageModel)
+
+    -- vggLSTM -> LSTM -> sequencer -> recursor -> LSTMcell -> nn.LSTM, init h0 with visual feature
+    self.LSTM.module.module.modules[1].prevOutput = self.visualFeatureRescaled
+
+    -- LSTM
+    self.output = self.LSTM:forward(input)
+    return self.output
 end
 
 function vggLSTM:backward(input, grad)
+    -- backprop the language model
+    local gradLSTMInput = self.LSTM:backward(self.visualFeatureRescaled, grad)
 
+    -- backprop the rescale visual feature layer
+    local gradVisualFeature = self.visualRescale:backward(self.outputImageModel, gradLSTMInput)
 end
+
+
